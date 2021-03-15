061562a276b358e42e787a1811e928b3eba9864b
Lilian Shen <lmshen2@linux-a1.ews.illinois.edu>
2021-03-14 19:55:30 -0500

    couldnt finish part 3

--- Lab7/count_disjoint_regions.s
+++ Lab7/count_disjoint_regions.s
@@ -34,5 +34,67 @@
 
 .globl count_disjoint_regions
 count_disjoint_regions:
-        # Your code goes here :)
-        jr      $ra
+    # Your code goes here :)
+    sub     $sp, $sp, 12         # callee save
+
+    sw      $s4, 0($sp)
+    sw      $s5, 4($sp)
+    sw      $s6, 8($sp)
+
+    move    $s4, $a0            # s4 = lines
+    move    $s5, $a1            # s5 = canvas
+    move    $s6, $a2            # s6 = solution
+
+    lw      $t0, 0($s4)         # t0 = num_lines
+    lw      $t1, 4($s1)         # t1 = int* coords[2]
+
+    li      $t2, 0              # t2 = i 
+for_loop:
+    bge     $t2, $t0, loop_end
+
+    addi    $t3, $t1, 4         # + memory address
+    lw      $t3, 0($t3)         # t3 = start_pos
+
+    addi    $t4, $0, 4          # int offset by 4
+    add     $t4, $t4, $t1       # + memory address
+    add     $t4, $t4, $t2       # + col 
+    lw      $t4, 0($t4)         # t4 = end_pos
+
+    move    $a0, $t3
+    move    $a1, $t4
+    move    $a2, $s5
+    jal     draw_line
+
+    andi    $t5, $t2, 1         # checks if odd or even
+    beq     $t5, 1, odd_iteration
+even_iteration:
+    li      $t5, 'A'            # A if even
+    j       skip
+odd_iteration:
+    li      $t5, 'B'            # B if odd
+skip:
+    div     $t6, $t2, 2
+    mfhi    $t6                 # mod operator
+    add     $t5, $t5, $t6
+
+    move    $a0, $t5
+    move    $a1, $s5
+
+    jal     count_disjoint_regions_step
+
+    lw      $t8, 4($s6)
+    mul     $t7, $t2, 4
+    add     $t7, $t8, $t7
+    sw      $v0, 0($t7)
+
+    j       for_loop
+    addi    $t2, $t2, 1
+loop_end:
+
+    lw      $s4, 0($sp)
+    lw      $s5, 4($sp)
+    lw      $s6, 8($sp)
+
+    add     $sp, $sp, 12
+
+    jr      $ra

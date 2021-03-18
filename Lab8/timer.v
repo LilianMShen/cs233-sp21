980828e1a2153282fc10797617f0454356379f3c
Lilian Shen <lmshen2@linux-a1.ews.illinois.edu>
2021-03-17 19:42:54 -0500

    debugging debugging debugging

--- Lab8/timer.v
+++ Lab8/timer.v
@@ -7,24 +7,24 @@ module timer(TimerInterrupt, cycle, TimerAddress,
     input         MemRead, MemWrite, clock, reset;
 
     wire [31:0] CycleCounterOut, InterruptCycleOut, AluOut;
-    wire InterruptLineOut, zero, negative, TimerRead, TimerWrite, Acknowledge;
-
-    register CycleCounter(CycleCounterOut, AluOUt, clock, 1'b1, reset);
-    register #(width = 32, reset_value = 32'hffffffff) InterruptCycle(InterruptCycleOut, data, clock, TimerWrite, reset);
-    register #(1) InterruptLine(InterruptLineOut, 1'b1, clock, InterruptLineEnable, InterruptLineReset);
-
-    alu32 alu(AluOut, zero, negative, 3'b010, CycleCounterOut, 32'b1);
-
-    tristate tri(cycle, CycleCounterOut, TimerRead);
+    wire InterruptLineOut, zero, negative, TimerRead, TimerWrite, Acknowledge, InterruptLineReset;
 
     wire InterruptLineEnable = CycleCounterOut == InterruptCycleOut;
     wire eqOne = 32'hffff001c == address;
     wire eqTwo = 32'hffff006c == address;
 
+    register CycleCounter(CycleCounterOut, AluOut, clock, 1'b1, reset);
+    register #(width(32), reset_value(32'hffffffff)) InterruptCycle(InterruptCycleOut, data, clock, TimerWrite, reset);
+    register #(1) InterruptLine(InterruptLineOut, 1'b1, clock, InterruptLineEnable, InterruptLineReset);
+
+    alu32 alu(AluOut, zero, negative, 3'b010, CycleCounterOut, 32'b1);
+
+    tristate t(cycle, CycleCounterOut, TimerRead);
+
     or o1(TimerAddress, eqOne, eqTwo);
-    or o2(InteruptLineReset, Acknowledge, reset);
+    or o2(InterruptLineReset, Acknowledge, reset);
 
     and a1(Acknowledge, eqOne, eqTwo);
     and a2(TimerRead, eqOne, MemRead);
-    and a3(TimeWrite, eqOne, MemWrite);
+    and a3(TimerWrite, eqOne, MemWrite);
 endmodule

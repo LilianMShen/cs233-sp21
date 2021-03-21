db3a29eb2b68532818b2f23054def78f74d2e061
Lilian Shen <lmshen2@linux-a1.ews.illinois.edu>
2021-03-20 22:59:56 -0500

    cp0 appears to be working

--- Lab8/cp0.v
+++ Lab8/cp0.v
@@ -14,4 +14,29 @@ module cp0(rd_data, EPC, TakenInterrupt,
     input         MTC0, ERET, TimerInterrupt, clock, reset;
 
     // your Verilog for coprocessor 0 goes here
+    wire [31:0] decoderOut, userStatus;
+    wire [29:0] m1Out;
+    wire exceptionLevel, o1Out, o2Out, a1Out, a2Out, n1Out;
+
+    wire [31:0] statusRegister = {16'b0, userStatus[15:8], 6'b0, exceptionLevel, userStatus[0]};
+    wire [31:0] causeRegister = {16'b0, TimerInterrupt, 15'b0};
+
+    decoder32 d(decoderOut, regnum, MTC0);
+
+    mux2v #(30) m1(m1Out, wr_data[31:2], next_pc, TakenInterrupt);
+    mux3v m2(rd_data, statusRegister, causeRegister, {EPC, 2'b0}, regnum);
+
+    register userS(userStatus, wr_data, clock, decoderOut[12], reset);
+    register #(1) exceptionL(exceptionLevel, 1'b1, clock, TakenInterrupt, o1Out);
+    register #(30) EPCR(EPC, m1Out, clock, o2Out, reset);
+
+    or o1(o1Out, reset, ERET); // Outputs to reset of exception register
+    or o2(o2Out, decoderOut[14], TakenInterrupt); // Outputs to enable of EPC register
+
+    and a1(a1Out, causeRegister[15], statusRegister[15]); // Goes into a3
+    and a2(a2Out, n1Out, statusRegister[0]); // Goes into a3
+    and a3(TakenInterrupt, a1Out, a2Out); // Outputs to TakenInterrupt
+
+    not n1(n1Out, statusRegister[1]);
+
 endmodule
--- Lab8/machine.v
+++ Lab8/machine.v
@@ -18,7 +18,11 @@ module machine(clk, reset);
    wire [31:0]  rd1_data, rd2_data, B_data, alu_out_data, load_data, wr_data;
 
    //Your extra wires go here
+   wire         TimerInterrupt, TimerAddress, TakenInterrupt, MemReadOut, MemWriteOut;
+   wire [31:0]  cycle, rd_data, wr_data_out;
+   wire [29:0]  EPC;
 
+   wire [31:0] takenInterruptMuxIn = 32'b80000180
 
    register #(30, 30'h100000) PC_reg(PC[31:2], next_PC[31:2], clk, /* enable */1'b1, reset);
    assign PC[1:0] = 2'b0;  // bottom bits hard coded to 00
@@ -45,6 +49,24 @@ module machine(clk, reset);
    mux2v #(5) rd_mux(wr_regnum, rt, rd, RegDst);
    
    //Connect your new modules below
-   
+
+   // rd2_data = c0:wr_data, 
+   cp0 c0(rd_data, EPC, TakenInterrupt, rd2_data, next_PC, MTC0, ERET, TimerInterrupt, clk, reset);
+
+   // rd2_data = t:data, alu_out_data = t:address,
+   timer tim(TimerInterrupt, cycle, TimerAddress, rd2_data, alu_out_data, MemRead, MemWrite, clk, reset);
+
+   mux2v eretMux(eretOut, next_PC, EPC, ERET);
+   mux2v #(30) takenInterruptMux(takenInterruptOut, eretOut, takenInterruptMuxIn[31:2], TakenInterrupt);
+   mux2v wr_dataMux(wr_data_out, wr_data, rd_data, MFC0);
+
+   assign wr_data = wr_data_out;
+
+   not notthething(NotIO, TimerAddress);
+   and aMemRead(MemReadOut, MemRead, NotIO);
+   and aMemWrite(MemWriteOut, NotIO, MemWrite);
+
+   assign MemRead = MemReadOut;
+   assign MemWrite = MemWriteOut;
 
 endmodule // machine

8bb6c02c8c5b1b09b26e469e8c171ef460c81aea
Lilian Shen <lmshen2@linux-a1.ews.illinois.edu>
2021-04-14 16:37:46 -0500

    added code, debugging now

--- Lab11/simplecache.cpp
+++ Lab11/simplecache.cpp
@@ -2,10 +2,38 @@
 
 int SimpleCache::find(int index, int tag, int block_offset) {
   // read handout for implementation details
-  return 0xdeadbeef;
+  // tag index offset
+  std::vector< SimpleCacheBlock > blockvector = _cache[index];
+  SimpleCacheBlock *block = nullptr;
+
+  for (int i = 0; i < blockvector.size(); i++){
+    if (blockvector[i].tag() == tag && blockvector[i].valid()) {
+      block = &blockvector[i];
+    }
+  }
+  
+  if (block == nullptr){
+    return 0xdeadbeef;
+  }
+
+  return (block->get_byte(block_offset));
 }
 
 void SimpleCache::insert(int index, int tag, char data[]) {
   // read handout for implementation details
   // keep in mind what happens when you assign (see "C++ Rule of Three")
+  std::vector< SimpleCacheBlock > blockvector = _cache[index];
+  bool flag = false;
+
+  for (int i = 0; i < blockvector.size(); i++){
+    if (!blockvector[i].valid()) {
+      blockvector[i].replace(tag, data);
+      flag = true;
+      break;
+    }
+  }
+
+  if (!flag) {
+    blockvector[0].replace(tag, data);
+  }
 }

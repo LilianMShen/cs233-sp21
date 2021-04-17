200766ebb756cdfd12f65aee45f69c8bc7af9d1b
Lilian Shen <lmshen2@linux-a1.ews.illinois.edu>
2021-04-17 16:52:45 -0500

    passing all unit tests

--- Lab11/cacheblock.cpp
+++ Lab11/cacheblock.cpp
@@ -2,5 +2,9 @@
 
 uint32_t Cache::Block::get_address() const {
   // TODO
-  return 0;
+  uint32_t numTagBits = _cache_config.get_num_index_bits();
+  uint32_t numOffsetBits = _cache_config.get_num_block_offset_bits();
+
+  return (_tag << (32 - numTagBits)) + (_index << numOffsetBits);
+
 }

fa52dbc789c7276bac7c2ce7091dfd13f678f180
Lilian Shen <lmshen2@linux-a1.ews.illinois.edu>
2021-04-17 16:44:55 -0500

    finished the cacheconfig.cpp and utils.cpp

--- Lab11/cacheconfig.cpp
+++ Lab11/cacheconfig.cpp
@@ -1,5 +1,6 @@
 #include "cacheconfig.h"
 #include "utils.h"
+#include <math.h>
 
 CacheConfig::CacheConfig(uint32_t size, uint32_t block_size, uint32_t associativity)
 : _size(size), _block_size(block_size), _associativity(associativity) {
@@ -7,4 +8,7 @@ CacheConfig::CacheConfig(uint32_t size, uint32_t block_size, uint32_t associativ
    * TODO
    * Compute and set `_num_block_offset_bits`, `_num_index_bits`, `_num_tag_bits`.
   */ 
+ _num_index_bits = log2((size / block_size) / associativity);
+ _num_block_offset_bits = log2(block_size);
+ _num_tag_bits = 32 - _num_index_bits - _num_block_offset_bits;
 }
--- Lab11/utils.cpp
+++ Lab11/utils.cpp
@@ -1,16 +1,21 @@
 #include "utils.h"
+#include <math.h>
 
 uint32_t extract_tag(uint32_t address, const CacheConfig& cache_config) {
   // TODO
-  return 0;
+  uint32_t tagBits = cache_config.get_num_tag_bits();
+  return address >> (32 - tagBits);
 }
 
 uint32_t extract_index(uint32_t address, const CacheConfig& cache_config) {
   // TODO
-  return 0;
+  uint32_t tagBits = cache_config.get_num_tag_bits();
+  uint32_t indexBits = cache_config.get_num_index_bits();
+  return (address >> (32 - tagBits - indexBits)) % (int)pow(2, indexBits);
 }
 
 uint32_t extract_block_offset(uint32_t address, const CacheConfig& cache_config) {
   // TODO
-  return 0;
+  uint32_t offsetBits = cache_config.get_num_block_offset_bits();
+  return address % (int) pow(2, offsetBits);
 }
